466ce7dd0e96774229866e0ae6023412aa4fb1f2
Carol <carolchristovamc@gmail.com>
2023-07-24 16:11:07 -0300

    feat: changing the result format number

--- app/src/main/java/com/example/temperature_converter/MainActivity.kt
+++ app/src/main/java/com/example/temperature_converter/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.temperature_converter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.temperature_converter.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        binding.buttonConverter.setOnClickListener{
+            val celsius=binding.editCelsius.text.toString().toDouble()
+            val fahrenheit= (celsius*1.8) + 32
+            val formattedResult = String.format("%.1f", fahrenheit)
+
+            binding.textResult.text="$formattedResult °F"
+        }
     }
 }
